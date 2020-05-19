f2cd6848ff4756caf6e86be1fad26645d2424abd
Victor Blasal <victor.blas@enti.cat>
2020-05-19 19:58:31 +0200

    shecpoint

--- code/src/physics.cpp
+++ code/src/physics.cpp
@@ -1,6 +1,6 @@
 #include <imgui\imgui.h>
 #include <imgui\imgui_impl_sdl_gl3.h>
-
+#include <vector>
 #include <glm\glm.hpp>
 #include <iostream>
 #include <glm\gtx\intersect.hpp>
@@ -26,12 +26,30 @@ namespace ClothMesh {
 	int w = 18;
 	int h = 14;
 	float initialSpaceBetweenPoints = 0.5f;
+	double totalTime;
+
+	struct Wave {
+		float amplitude = 0.5f; //alçada centre a pic
+		float lambda = 2.f; //distancia entre pics
+		float frequency = 2; //pics per segon
+		glm::vec3 direction = glm::vec3(1,0,0);
+
+		Wave() {}
+		Wave(float a, float l, float f, glm::vec3 dir) {
+			amplitude = a;
+			lambda = l;
+			frequency = f;
+			direction = dir;
+		}
+	};
 
 	class Mesh {
 	private:
 		
 		glm::vec3 *positions1D;
 		glm::vec3 **positions;
+		glm::vec3 **initialPositions;
+		std::vector<Wave> *waves;
 
 	public:
 		glm::vec3 *getPositions1D() {
@@ -39,6 +57,9 @@ namespace ClothMesh {
 		}
 		glm::vec3 **getPositions() {
 			return positions;
+		}
+		std::vector<Wave> *getWaves() {
+			return waves;
 		}
 
 		void setPositions1D() {
@@ -49,21 +70,28 @@ namespace ClothMesh {
 		}
 
 		void reset() {
+			waves->clear();
 			for (int i = 0; i < w; i++) {
 				for (int j = 0; j < h; j++) {
-					positions[i][j].x = i * initialSpaceBetweenPoints - (w-1) * 0.5f * initialSpaceBetweenPoints ; 
-					positions[i][j].y = 5;
-					positions[i][j].z = j * initialSpaceBetweenPoints - (h-1) * 0.5f * initialSpaceBetweenPoints ;
+					initialPositions[i][j].x = i * initialSpaceBetweenPoints - (w-1) * 0.5f * initialSpaceBetweenPoints ;
+					initialPositions[i][j].y = 5;
+					initialPositions[i][j].z = j * initialSpaceBetweenPoints - (h-1) * 0.5f * initialSpaceBetweenPoints ;
+					positions[i][j] = initialPositions[i][j];;
 				}
 			}
+			waves->push_back(Wave());
+			totalTime = 0;
 		}
 
 		Mesh() {
 			positions1D = new glm::vec3[w*h];
 			positions = new glm::vec3*[w];
+			initialPositions = new glm::vec3*[w];
 			for (int i = 0; i < w; i++) {
 				positions[i] = new glm::vec3[h];
+				initialPositions[i] = new glm::vec3[h];
 			}
+			waves = new std::vector<Wave>();
 			reset();
 		}
 	};
@@ -85,7 +113,18 @@ void PhysicsInit() {
 
 void PhysicsUpdate(float dt) {
 
+	for (int w = 0; w < myPM->getWaves()->size(); w++) {
+		for (int i = 0; i < ClothMesh::w; i++) {
+			for (int j = 0; j < ClothMesh::h; j++) {
 
+			}
+		}
+	}
+	
+
+	ClothMesh::totalTime += dt;
+
+	//update Mesh
 	myPM->setPositions1D();
 	ClothMesh::updateClothMesh(&(myPM->getPositions1D()[0].x));
 
