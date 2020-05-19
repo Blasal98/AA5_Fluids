86a28cef6305f9ac10a1dc8c710bd0db128ceddb
Victor Blasal <victor.blas@enti.cat>
2020-05-19 19:25:32 +0200

    se printa

--- code/src/physics.cpp
+++ code/src/physics.cpp
@@ -1,33 +1,112 @@
 #include <imgui\imgui.h>
 #include <imgui\imgui_impl_sdl_gl3.h>
 
+#include <glm\glm.hpp>
+#include <iostream>
+#include <glm\gtx\intersect.hpp>
+
 //Exemple
 extern void Exemple_GUI();
 extern void Exemple_PhysicsInit();
 extern void Exemple_PhysicsUpdate(float dt);
 extern void Exemple_PhysicsCleanup();
-
 bool show_test_window = false;
+
+
+extern bool renderSphere;
+extern bool renderCloth;
+
+namespace ClothMesh {
+	extern void setupClothMesh();
+	extern void cleanupClothMesh();
+	extern void updateClothMesh(float* array_data);
+	extern void drawClothMesh();
+
+
+	int w = 18;
+	int h = 14;
+	float initialSpaceBetweenPoints = 0.5f;
+
+	class Mesh {
+	private:
+		
+		glm::vec3 *positions1D;
+		glm::vec3 **positions;
+
+	public:
+		glm::vec3 *getPositions1D() {
+			return positions1D;
+		}
+		glm::vec3 **getPositions() {
+			return positions;
+		}
+
+		void setPositions1D() {
+			for (int i = 0; i < w; i++) {
+				for (int j = 0; j < h; j++)
+					positions1D[i * h + j] = glm::vec3(positions[i][j].x, positions[i][j].y, positions[i][j].z);
+			}
+		}
+
+		void reset() {
+			for (int i = 0; i < w; i++) {
+				for (int j = 0; j < h; j++) {
+					positions[i][j].x = i * initialSpaceBetweenPoints - (w-1) * 0.5f * initialSpaceBetweenPoints ; 
+					positions[i][j].y = 5;
+					positions[i][j].z = j * initialSpaceBetweenPoints - (h-1) * 0.5f * initialSpaceBetweenPoints ;
+				}
+			}
+		}
+
+		Mesh() {
+			positions1D = new glm::vec3[w*h];
+			positions = new glm::vec3*[w];
+			for (int i = 0; i < w; i++) {
+				positions[i] = new glm::vec3[h];
+			}
+			reset();
+		}
+	};
+}
+ClothMesh::Mesh *myPM;
+
+
+
+void PhysicsInit() {
+	//renderSphere = true;
+	renderCloth = true;
+
+	myPM = new ClothMesh::Mesh();
+
+	ClothMesh::setupClothMesh();
+
+
+}
+
+void PhysicsUpdate(float dt) {
+
+
+	myPM->setPositions1D();
+	ClothMesh::updateClothMesh(&(myPM->getPositions1D()[0].x));
+
+
+}
+
+void PhysicsCleanup() {
+
+	ClothMesh::cleanupClothMesh();
+
+
+}
+
 void GUI() {
 	bool show = true;
 	ImGui::Begin("Physics Parameters", &show, 0);
 
-	{	
+	{
 		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);//FrameRate
 		Exemple_GUI();
 	}
-	
+
 	ImGui::End();
-}
-
-void PhysicsInit() {
-	Exemple_PhysicsInit();
-}
-
-void PhysicsUpdate(float dt) {
-	Exemple_PhysicsUpdate(dt);
-}
-
-void PhysicsCleanup() {
-	Exemple_PhysicsCleanup();
 }
